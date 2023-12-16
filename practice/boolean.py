ce62efddbf811d12df835b429e2a9567312a8e0f
just_laizy <orazow.dawut.08@gmail.com>
2023-12-16 21:16:27 +0500

    1.0.1

--- practice/boolean.py
+++ practice/boolean.py
@@ -11,7 +11,32 @@
 # b = int(input("Give the number : "))
 # print(a >2 and b<=3)
 
-# bool4
+# bool5
 # a = int(input("Give the number a: "))
 # b = int(input("Give the number b: "))
 # print(a>=0 and b < -2)
+
+# bool16
+# a = int(input("Give num : "))
+# print(a>9 and a<100 and a%2==0)
+
+# bool17 
+# a = int(input("Give num : "))
+# print(a>99 and a<1000 and a%2==1)
+
+# bool18
+# a = int(input("Give num : "))
+# b = int(input("Give num : "))
+# c = int(input("Give num : "))
+# a = a**2
+# b = b**2
+# c = c**2
+# print(a == b or c == a or c == b)
+
+# bool20
+# a = int(input("Give num : "))
+# b = a//100
+# c = (a//10)%10
+# d = a%10
+
+# print(b != c and c != d and d != b)
