dcded2687cf34d5ccc341dc1a2ff6f57027017c1
just_laizy <orazow.dawut.08@gmail.com>
2023-12-25 21:11:14 +0500

    1.2.3

---
+++ cource_list/if.py
@@ -0,0 +1,15 @@
+age = 45
+teen = 20
+
+x = 1
+y = 2
+
+if age > teen:
+    print("you are not a teenager")
+if x != y:
+    print(f"{x} is  not equal to {y}")    
+
+age = 20
+
+if age == teen:
+    print("You are too young ")
---
+++ practice/if.py
@@ -0,0 +1,41 @@
+# # if 1
+
+# # x = int(input("> "))
+
+# # if x>0:
+# #     print(x+1)
+# # else : 
+# #     print(x)
+
+
+# # if x>0:
+# #     print(x+1)
+# # elif x == 0:
+# #     x=10
+# #     print(x)
+# # else:
+# #     print(x-2)
+
+# a = int(input("> "))
+# b = int(input("> "))
+
+# # if a > b:
+# #     print(f'{a} is bigger than {b}')
+# # if a < b:
+# #     print(f'{b} is bigger than {a}')\
+
+# p=0
+
+# if (a or b) > 0:
+#     p+=1
+
+a=int(input('a=>'))
+b=int(input('b=>'))
+c=int(input('c=>'))
+
+if a>c and c>b:
+    print(c)
+# elif (b>a and a>c):
+#     print(a)
+# elif (a>b and b>c):
+#     print(b)
